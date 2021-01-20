6f40797dd9b0acfcf04830d159e6178ff62457bb
glowing713 <glowing713@naver.com>
2021-01-20 22:53:18 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -77,55 +77,55 @@ def is_no(one_more_input):
 def main():
     next_game = True
     print("Play Baseball")
-
-    # ########
-    # 3 스트라이크를 하고 다음 게임을 이어가지 않거나
-    # 게임 도중 0을 입력할 때까지 게임은 무한 반복
-    # #########
     while next_game:
-        user_input = 999
+        user_input = 999    # 사용자 입력 초기화
+
+        # 맞춰야 할 랜덤 숫자 할당
         random_number = str(get_not_duplicated_three_digit_number())
-        print("Random Number is : ", random_number)
-        ######### 3 스트라이크가 나올 때까지 반복하는 부분 #########
+        print("Random Number is : ", random_number)  # 할당된 랜덤 숫자 출력
+
         while True:
             user_input = input('Input guess number : ')
-            # 게임 도중 0을 입력하면 즉시 종료
+            # <특수조건> 사용자가 게임 중 0을 입력하면 게임을 종료해야 한다.
             if user_input == '0':
-                break
-
-            # 사용자의 올바른 입력 유도
-            while not is_validated_number(user_input):
-                print('Wrong Input, Input again')
-                user_input = input('Input guess number : ')
-
-            # 스트라이크, 볼 판정
-            strike, ball = get_strikes_or_ball(user_input, random_number)
-            print(f'Strikes : {strike} , Balls : {ball}')
-            # 3 스트라이크에 종료
-            if strike == 3 and ball == 0:
-                break
-
-        # 게임 도중에 사용자가 0을 입력해서 종료된 경우, 완전히 게임을 멈춘다.
-        if user_input == '0':
-            break
-
-        ######### 다음 게임 진행 여부를 올바르게 입력할 때까지 반복하는 부분 #########
-        next_game_str = ''
-        while True:
-            next_game_str = input('You win, one more(Y/N)?')
-            if next_game_str == '0':
-                break
-            elif is_yes(next_game_str):
-                next_game = True
-                break
-            elif is_no(next_game_str):
                 next_game = False
                 break
-            else:
+
+            # 잘못된 숫자를 입력했다면 오류메세지 출력하고 다시 입력을 받는다.
+            if not is_validated_number(user_input):
                 print('Wrong Input, Input again')
-        # 0을 입력받으면 완전히 종료한다.
-        if next_game_str == '0':
-            break
+
+            # 제대로 입력. 야구게임 연산 시작
+            else:
+
+                strike, ball = get_strikes_or_ball(user_input, random_number)
+                if strike == 3 and ball == 0:
+                    # 정답을 맞춤
+                    want_next = input('You win, one more(Y/N)?')
+                    # <특수조건> 사용자가 게임 중 0을 입력하면 게임을 종료해야 한다.
+                    if user_input == '0':
+                        next_game = False
+                        break
+                    # 게임을 더 할 의향이 있는지 물어보기. 문법 틀리게 입력하면 다시 입력받기.
+                    while True:
+                        # <특수조건> 사용자가 게임 중 0을 입력하면 게임을 종료해야 한다.
+                        if want_next == '0':
+                            next_game = False
+                            break
+                        elif is_yes(want_next):
+                            next_game = True
+                            break
+                        elif is_no(want_next):
+                            next_game = False
+                            break
+                        else:
+                            print('Wrong Input, Input again')
+                            want_next = input('You win, one more(Y/N)?')
+                    break
+
+                else:
+                    print(f'Strikes : {strike} , Balls : {ball}')
+
     print("Thank you for using this program")
     print("End of the Game")
 
